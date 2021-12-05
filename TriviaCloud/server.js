f734e8982747d2bb3da9a745779d091afe8be826
Mansib Rahman <60615362+mvnsib@users.noreply.github.com>
2021-12-05 00:38:14 +0000

    Update server.js

--- TriviaCloud/server.js
+++ TriviaCloud/server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname) + '/views/index.html');
 });
 
-const PORT =  proces.env.PORT || 8080;
+const PORT =  process.env.PORT || 8080;
 app.listen(PORT, _ => {
     console.log('App deployed at ', {PORT});
 });
